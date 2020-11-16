8fef88a090ea45ab8bf24a7639f6c51d25466fca
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-16 13:03:28 +0000

    Find largest contour in the image This should represent the table region

--- main.py
+++ main.py
@@ -18,6 +18,29 @@ def preProcess(img):
     return threshold
 
 
+def findLargestQuadrilateralContour(contours):
+    biggest_area = 0
+    biggest_contour = None
+    for contour in contours:
+        # Get the area of this contour
+        area = cv2.contourArea(contour)
+
+        # Get the length of the perimeter
+        perimeter = cv2.arcLength(contour, True)
+
+        # Approximate a shape that resembles the contour
+        # This is needed because the image might be warped, thus
+        # edges are curved and not perfectly straight
+        approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+
+        # Check if area is bigger than previous contour
+        # and if the approximation contains only 4 sides (i.e. quadrilateral)
+        if area > biggest_area and len(approx) == 4:
+            biggest_area = area
+            biggest_contour = contour
+    return [biggest_contour]
+
+
 def main():
     # Read image
     img = cv2.imread("sample_table.jpg")
@@ -26,8 +49,21 @@ def main():
     # Process image
     processed = preProcess(img_copy)
 
+    # Find contours
+    contours, _ = cv2.findContours(processed, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find table region
+    # It is assumed the table takes up most of the image, thus it
+    # can be identified by finding the largest contour with 4 sides
+    table_contour = findLargestQuadrilateralContour(contours)
+
+    # FOR DEBUG PURPOSES ONLY
+    # Create new image with table contour displayed on top of processed image
+    table_contour_image = cv2.cvtColor(processed.copy(), cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(table_contour_image, table_contour, -1, (0, 0, 255), 10)
+
     # Show image
-    plt.imshow(cv2.cvtColor(processed, cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(table_contour_image, cv2.COLOR_BGR2RGB))
     plt.show()
 
 
